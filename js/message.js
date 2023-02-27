8014e49949262a0d2f55036f143125833b35b49b
Zea <34760009+shengxiawuwu@users.noreply.github.com>
2023-02-27 16:33:47 +0800

    Delete js directory

--- js/message.js
+++
@@ -1,9 +0,0 @@
-function messageTip(msg, duration = 3000) {
-    let elem = document.createElement('div');
-    elem.className = "errorTip";
-    elem.innerText = msg;
-    document.body.appendChild(elem);
-    setTimeout(() => {
-        document.body.removeChild(elem);
-    }, duration)
-}
